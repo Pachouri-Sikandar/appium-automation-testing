8ed77703d3c501647bc135b8c979a14b09b9738b
Pachouri-Sikandar <ankitpachouri0891@gmail.com>
2017-05-05 13:43:35 +0530

    Initial commit

---
+++ src/test/java/TestWeatherApp.java
@@ -0,0 +1,27 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+
+/**
+ * Created by ankit on 3/10/17.
+ */
+public class TestWeatherApp {
+
+    @Before
+    public void setUp() throws MalformedURLException {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    //Write your test here
+    @Test
+    public void testMethod() throws InterruptedException {
+
+    }
+}
